05e1a4cb00f197b820ab635f07b380a42dd53cbe
Tom Douglas <tomdoug@gmail.com>
2019-08-24 08:47:28 -0500

    Init

---
+++ src/Todo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Todo.css';
+
+function Todo() {
+  return (
+    
+  );
+}
+
+export default Todo;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './Todo';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<Todo />, document.getElementById('root'));
